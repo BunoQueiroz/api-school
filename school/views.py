a00dc6f89931fae75215c10711241e42cd96c4bf
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-18 14:03:51 -0300

    Criando os modelos ViewSets dos modelos de curso, aluno e matrícula

--- school/views.py
+++ school/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from school.models import Course, Registration, Studant
+from school.serializers import CourseSerializer, RegistrationSerializer, StudantSerializer
 
-# Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    """Disponibiliza o CRUD para todos os Cursos"""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    """Disponibiliza o CRUD para todas as Matrículas"""
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+
+class StudantViewSet(viewsets.ModelViewSet):
+    """Disponibiliza o CRUD para todos os Estudantes"""
+    queryset = Studant.objects.all()
+    serializer_class = StudantSerializer
