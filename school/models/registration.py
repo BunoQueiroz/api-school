6f119bf188a2d8c0b3b5688cb018e8c4cf7c25f6
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-18 13:57:13 -0300

    Impedindo que os campos de metrículas sejam vazios

--- school/models/registration.py
+++ school/models/registration.py
@@ -2,10 +2,10 @@ from django.db import models
 from school.models import Studant, Course
 
 class Registration(models.Model):
-    studant = models.ForeignKey(Studant, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    initial_date = models.DateField()
-    deadline = models.DateField()
+    studant = models.ForeignKey(Studant, on_delete=models.CASCADE, blank=False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=False)
+    initial_date = models.DateField(blank=False)
+    deadline = models.DateField(blank=False)
 
     def __str__(self):
         return f'{self.studant} - {self.course}'
