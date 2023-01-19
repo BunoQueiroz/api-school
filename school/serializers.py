66bb7b1cc643791da1d372e125974d0f30ef6f32
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-01-19 09:39:33 -0300

    Criando a classe para serealizar os cursos em que um aluno está matriculado

--- school/serializers.py
+++ school/serializers.py
@@ -36,3 +36,11 @@ class StudantPerCourseSerializers(serializers.ModelSerializer):
     class Meta:
         model = Registration
         exclude = ['course', ]
+
+class CoursesPerStudantSerializer(serializers.ModelSerializer):
+    initial_date = serializers.DateField(format='%d-%m-%Y')
+    deadline = serializers.DateField(format='%d-%m-%Y')
+    course = serializers.ReadOnlyField(source='course.description')
+    class Meta:
+        model = Registration
+        exclude = ['studant', ]
